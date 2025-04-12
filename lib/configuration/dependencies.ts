dc7640ae99b74bab48d240ede9aaa2d240569701
Leo <liyung.chen@tum.de>
2025-04-12 19:32:21 +0200

    update connection arn

--- lib/configuration/dependencies.ts
+++ lib/configuration/dependencies.ts
@@ -1,6 +1,6 @@
 // GitHub
 export const GITHUB_CONNECTION_ARN =
-    "arn:aws:codeconnections:us-east-1:086026665560:connection/75ba992d-a6cd-424c-a0a9-9d9df4bfabc6";
+    "arn:aws:codeconnections:us-east-1:669131042313:connection/07ce8156-2abd-4a93-84b4-89d4e39554a0";
 export const GITHUB_REPO = "TaiGerPortalEmailService";
 export const GITHUB_OWNER = "LIYUNG";
 export const GITHUB_PACKAGE_BRANCH = "main";
